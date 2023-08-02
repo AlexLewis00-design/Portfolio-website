f8953e26d5c3193f209ef37e940fefc682278adb
Alex Lewis <aloulewis06@gmail.com>
2023-08-02 14:12:32 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+/*On contact page text  'thank you for visiting' - to change to 'have a great day' when mouse hovers and back when mouse out*/
+function mOver(obj) {
+obj.innerHTML = "Have a great day!"
+}
+function mOut(obj) {
+obj.innerHTML = "Thank you for visiting"
+}
+
